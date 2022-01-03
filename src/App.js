7014be0fc3d8c8ce7b43015dc37a7dcffd26c87d
Kratoshome <kratoshome101@gmail.com>
2022-01-03 15:53:21 +0200

    add reduser

--- src/App.js
+++ src/App.js
@@ -4,20 +4,24 @@ import Header from "./components/Header/Header";
 import "./index.css";
 import HomePage from "./pages/HomePages/HomePages";
 import NotFound from "./pages/NotFound/NotFound";
+import { Provider } from "react-redux";
+import { store } from "./redux";
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Header />
-      </BrowserRouter>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <BrowserRouter>
+          <Header />
+        </BrowserRouter>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </Provider>
   );
 }
 
