a9e573059aa443b1cda83abdebc5f694ca8a1903
Kratoshome <kratoshome101@gmail.com>
2022-01-04 13:45:21 +0200

    delet product in menu cart

--- src/components/GameBuy/GameBuy.js
+++ src/components/GameBuy/GameBuy.js
@@ -1,21 +1,30 @@
 import React from "react";
 import Button from "../Button/Button";
 import "./GameBuy.css";
-import { useDispatch } from "react-redux";
-import { setItemInCart } from "../../redux/cart/reduse";
+import { useDispatch, useSelector } from "react-redux";
+import { deleteItemFromCart, setItemInCart } from "../../redux/cart/reduse";
 
 const GameBuy = ({ game }) => {
   const dispatch = useDispatch();
+  const items = useSelector((state) => state.cart.itemsInCart);
+  const isItemInCart = items.some((item) => item.id === game.id);
 
   const handeleClick = (e) => {
     e.stopPropagation();
-    dispatch(setItemInCart(game));
+    if (isItemInCart) {
+      dispatch(deleteItemFromCart(game.id));
+    } else {
+      dispatch(setItemInCart(game));
+    }
   };
   return (
     <div className="GameBuy">
       <span className="gamePrise">{game.price} грн.</span>
-      <Button type="primery" onClick={handeleClick}>
-        В корзину
+      <Button
+        type={isItemInCart ? "secondary" : "primery"}
+        onClick={handeleClick}
+      >
+        {isItemInCart ? "Убрать из корзинны" : "В корзину"}
       </Button>
     </div>
   );
